38d3e478a5323ee84c6d3e5ff42fb0931f4cd607
200730946 <200730946@PO-24849>
2024-09-10 11:16:43 -0400

    etape 3 fin

--- JeuGladiateurs/src/combat/AffichageEcran.java
+++ JeuGladiateurs/src/combat/AffichageEcran.java
@@ -7,21 +7,22 @@ public class AffichageEcran {
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
-    // <editor-fold defaultstate="collapsed" desc="MÃ©thodes d'affichage">
+    // <editor-fold defaultstate="collapsed" desc="Méthodes d'affichage">
     public void afficherDebutCombat() {
-        // TODO : Afficher le message du dÃ©but du combat, tel que montrÃ© dans l'Ã©noncÃ©
+        // TODO : Afficher le message du début du combat, tel que montré dans l'énoncé
+        System.out.println("\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n>>>>>> Que le combat commence !>>>>>\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
     }
     
     public void afficherSeparateurDeTour() {
-        // TODO : Afficher un retour Ã  la ligne, suivi d'une ligne d'astÃ©risques, tel que montrÃ© dans l'Ã©noncÃ©
+        // TODO : Afficher un retour à la ligne, suivi d'une ligne d'astérisques, tel que montré dans l'énoncé
     }
 
     public void afficherSeparateurInfosPerso() {
-        // TODO : Afficher un retour Ã  la ligne, suivi du sÃ©parateur "infos personnages", tel que montrÃ© dans l'Ã©noncÃ©
+        // TODO : Afficher un retour à la ligne, suivi du séparateur "infos personnages", tel que montré dans l'énoncé
     }
 
     public void afficheVictoire(Personnage personnage1, Personnage personnage2) {
-        // TODO : Afficher le gagnant du combat, tel que montrÃ© dans l'Ã©noncÃ©
+        // TODO : Afficher le gagnant du combat, tel que montré dans l'énoncé
     }
     // </editor-fold>
 }
