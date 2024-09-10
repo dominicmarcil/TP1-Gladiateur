9b495f57e13be0c6a3da6633b9e5c0a707f85816
200730946 <200730946@PO-24849>
2024-09-10 11:26:04 -0400

    etape 4 fin

--- JeuGladiateurs/src/combat/CompteurDeTour.java
+++ JeuGladiateurs/src/combat/CompteurDeTour.java
@@ -7,6 +7,7 @@ public class CompteurDeTour {
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Attributs">
     // TODO : Mettre vos attributs ici
+    public int cptrTour;
     // </editor-fold>
     
     // **************************************************************************
@@ -15,6 +16,7 @@ public class CompteurDeTour {
     // <editor-fold defaultstate="collapsed" desc="Constructeurs et initialisation">
     public CompteurDeTour() {
         // TODO : Constructeur SANS paramètres qui initialise un compteur de tour (attribut) à 1
+        this.cptrTour=1;
     }
     // </editor-fold>
 
@@ -23,19 +25,28 @@ public class CompteurDeTour {
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Getters et setters">
     // TODO : Le getter
-    // TODO : Le setter
-    // </editor-fold>
+    public int getCptrTour() {    
+        return cptrTour;
+    }
 
+    // TODO : Le setter
+    public void setCptrTour(int cptrTour) {    
+        this.cptrTour = cptrTour;
+    }
+
+    // </editor-fold>
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Mécanique de jeu">
     public void augmenteTour() {
         // TODO : Incrémenter le compteur de tour
+        this.cptrTour++;
     }
 
     public void afficheTour() {
         // TODO : Afficher le tour actuel, tel que montré dans l'énoncé
+        System.out.println("---------------\nTOUR ACTUEL: "+cptrTour+"\n---------------");
     }
     // </editor-fold>
 }
