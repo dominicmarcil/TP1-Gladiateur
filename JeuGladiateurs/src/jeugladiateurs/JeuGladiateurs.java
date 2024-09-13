312083bc45323874ffed07f59919def02ecba4de
200730946 <200730946@PO-24849>
2024-09-13 14:49:28 -0400

    Partie 2 Rétiaire fin

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -4,41 +4,41 @@ import personnages.Personnage;
 import combat.CompteurDeTour;
 import combat.AffichageEcran;
 import personnages.Mirmillon;
+import personnages.Rétiaire;
 
 public class JeuGladiateurs {
 
     public static void main(String[] args) {
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
-    // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
-    CompteurDeTour tour = new CompteurDeTour();
-    AffichageEcran affichage = new AffichageEcran();
-    Personnage Bob = new Mirmillon("Bob le malchanceux",15,15,70,15);
-    Personnage Igor = new Personnage("Igor l'empaleur",25,5,100,30);
-    // </editor-fold>
+        // **************************************************************************
+        // **************************************************************************
+        // **************************************************************************
+        // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
+        CompteurDeTour tour = new CompteurDeTour();
+        AffichageEcran affichage = new AffichageEcran();
+        Personnage Bob = new Mirmillon("Bob le malchanceux", 15, 15, 70, 15);
+        Personnage Igor = new Rétiaire("Igor l'empaleur", 25, 5, 100, 30);
+        // </editor-fold>
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
-    // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
-    // TODO : Afficher les infos de chaque personnage
-    Bob.afficherInfosPersonnage();
-    Igor.afficherInfosPersonnage();
-    // TODO : Afficher le message du début du combat
-    affichage.afficherDebutCombat();
-    Bob.setNewInitiativeRandom();
-    Igor.setNewInitiativeRandom();
-        while (Bob.pointsDeVie>0 && Igor.pointsDeVie>0){
+        // **************************************************************************
+        // **************************************************************************
+        // **************************************************************************
+        // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
+        // TODO : Afficher les infos de chaque personnage
+        Bob.afficherInfosPersonnage();
+        Igor.afficherInfosPersonnage();
+        // TODO : Afficher le message du début du combat
+        affichage.afficherDebutCombat();
+        Bob.setNewInitiativeRandom();
+        Igor.setNewInitiativeRandom();
+        while (Bob.pointsDeVie > 0 && Igor.pointsDeVie > 0) {
             tour.afficheTour();
             for (int i = 0; i < 100; i++) {
-                if (Bob.initiative==i) {
+                if (Bob.initiative == i) {
                     Bob.frapperPersonnage(Igor);
-                    
-                    
+
                 }
-                if (Igor.initiative==i) {
+                if (Igor.initiative == i) {
                     Igor.frapperPersonnage(Bob);
                 }
             }
@@ -52,17 +52,14 @@ public class JeuGladiateurs {
         }
         affichage.afficheVictoire(Bob, Igor);
 
-    
-    
-    // </editor-fold>
-
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
-    // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
-    // TODO : La boucle contenant les étapes du combat
-    // TODO : Après la boucle, afficher le résultat du combat
-    // </editor-fold>
+        // </editor-fold>
+        // **************************************************************************
+        // **************************************************************************
+        // **************************************************************************
+        // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
+        // TODO : La boucle contenant les étapes du combat
+        // TODO : Après la boucle, afficher le résultat du combat
+        // </editor-fold>
     }
 
 }
