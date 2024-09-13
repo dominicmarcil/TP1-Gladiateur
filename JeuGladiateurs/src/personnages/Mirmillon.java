61c04a475fe56b2f1afaf7320f1e154cac6a4b86
200730946 <200730946@PO-24849>
2024-09-13 14:07:17 -0400

    Partie 2 mirmillon fin

---
+++ JeuGladiateurs/src/personnages/Mirmillon.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personnages;
+
+
+/**
+ *
+ * @author 200730946
+ */
+public class Mirmillon extends Personnage {
+    String classe="Mirmillon";
+    
+    public Mirmillon(String nom, int attaqueMax, int defense, int pvs, int ini){
+//        super.initiative=ini;
+//        super.nom=nom;
+//        super.pointsDeVie=pvs;
+//        super.valeurDefense=defense;
+//        super.valeurMaxAttaque=attaqueMax;
+super(nom,attaqueMax,defense,pvs,ini);
+    }
+
+@Override
+public void afficherInfosPersonnage() { 
+        // TODO : Afficher les infos du personnage, tel que montré dans l'énoncé
+        System.out.println("\n"+nom+"\n\tClasse : "+classe+"\n\tAttaque : "+valeurMaxAttaque+"\n\tDéfense : "+valeurDefense+"\n\tPoints de vie : "+pointsDeVie+"\n\tInitiative : "+initiative);
+        
+        if (pointsDeVie<=0) {
+        System.out.println("\tStatut: Mort");
+        }
+        else {
+          System.out.println("\tStatut: Vivant");    
+        }
+            
+        }
+    
+@Override
+public void setNewInitiativeRandom() {
+        // TODO : Modifier de façon aléatoire la valeur INI du personnage.
+        
+        this.initiative=(int)(Math.random()*(31));
+    }
+}
