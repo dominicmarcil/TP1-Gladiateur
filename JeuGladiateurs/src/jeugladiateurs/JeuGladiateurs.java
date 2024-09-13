e57a8446f14a4d4dd0aae06d28922e79164f2a7f
200730946 <200730946@PO-24849>
2024-09-13 14:07:27 -0400

    Partie 2 mirmillon fin

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -3,6 +3,7 @@ package jeugladiateurs;
 import personnages.Personnage;
 import combat.CompteurDeTour;
 import combat.AffichageEcran;
+import personnages.Mirmillon;
 
 public class JeuGladiateurs {
 
@@ -14,7 +15,7 @@ public class JeuGladiateurs {
     // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
     CompteurDeTour tour = new CompteurDeTour();
     AffichageEcran affichage = new AffichageEcran();
-    Personnage Bob = new Personnage("Bob le malchanceux",15,15,70,15);
+    Personnage Bob = new Mirmillon("Bob le malchanceux",15,15,70,15);
     Personnage Igor = new Personnage("Igor l'empaleur",25,5,100,30);
     // </editor-fold>
 
@@ -27,11 +28,21 @@ public class JeuGladiateurs {
     Igor.afficherInfosPersonnage();
     // TODO : Afficher le message du début du combat
     affichage.afficherDebutCombat();
+    Bob.setNewInitiativeRandom();
+    Igor.setNewInitiativeRandom();
         while (Bob.pointsDeVie>0 && Igor.pointsDeVie>0){
             tour.afficheTour();
             for (int i = 0; i < 100; i++) {
                 if (Bob.initiative==i) {
                     Bob.frapperPersonnage(Igor);
+                    if (Igor.pointsDeVie==0) {
+                     System.out.println("\nBob décapite son adversaire!");
+                    }
+                    else{
+                        System.out.println("\nBob attaque à nouveau!");
+                        Bob.frapperPersonnage(Igor);
+                    }
+                    
                 }
                 if (Igor.initiative==i) {
                     Igor.frapperPersonnage(Bob);
